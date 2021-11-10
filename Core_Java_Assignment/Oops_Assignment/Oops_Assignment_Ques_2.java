73e42bffefef03e408a1a7a421723e7147b6aa41
megha-nancy <93337319+megha-nancy@users.noreply.github.com>
2021-11-10 10:17:05 +0530

    Update Oops_Assignment_Ques_2.java

--- Core_Java_Assignment/Oops_Assignment/Oops_Assignment_Ques_2.java
+++ Core_Java_Assignment/Oops_Assignment/Oops_Assignment_Ques_2.java
@@ -1,44 +1,44 @@
-public class Bank {
-
-	public static void main(String[] args) {
-   SavingAccount savingaccount = new SavingAccount(20000,1000);
-   CurrentAccount currentaccount = new CurrentAccount(20000,3000);
-   savingaccount.totalAmount();
-   currentaccount.totalAmount();
-   double totalbankbalance= savingaccount.totalAmount()+currentaccount.totalAmt();
-   System.out.println("total cash in the bank:"+totalbankbalance);
-   }
-
-}
-
-public class SavingAccount extends Bank{
-	    private double fixed_Deposit;
-	    private double balance;
-	    
-
-	    public SavingAccount(double fixed_Deposit, double balance)
-	    {
-	    	this.fixed_Deposit=fixed_Deposit;
-	    	this.balance=balance;
-	    }
-	    public double totalAmount() {
-	    	double Savingbalance= fixed_Deposit+balance;
-	    	return Savingbalance;
-	   }
-}
-public class CurrentAccount extends Bank {
-	private double cash_credit;
-	private double balance;
-	
- public CurrentAccount(double cash_credit, double balance)
- {
-	 this.balance=balance;
-	 this.cash_credit=cash_credit;
-	 
- }
- public double totalAmount() {
-
-     double currentbalance= cash_credit+balance;
-     return currentbalance;
-      }
+class Employee{
+   void sal(){
+       }
+       }
+class Manager extends Employee
+      {
+	int managerSalary;
+	int incentive;
+	Manager(int managerSalary,int incentive)
+	{
+		this.managerSalary=managerSalary;
+		this.incentive=incentive;
+		
 	}
+	public void salary()
+	{
+		System.out.println("the salary of manager is: "+(managerSalary+incentive));
+	}
+       }
+class Labour extends Employee
+      {
+	int labourSalary;
+	int overtime;
+	Labour(int labourSalary,int overtime)
+	{
+		this.labourSalary=labourSalary;
+		this.overtime=overtime;
+		
+	}
+	public void salary()
+	{
+		System.out.println("the salary of labour is: "+(labourSalary+overtime));
+	}
+      }
+public class  EmployeeMain
+      {
+	public static void main(String[] args)
+	{
+		Manager manager = new Manager(15000,5000);
+		Labour labour = new Labour(1000,500);
+		manager.salary();
+		labour.salary();
+	}
+      }
