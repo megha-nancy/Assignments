e6b134818ab722c64b1b6e32a71cf4f8d25aeed7
megha-nancy <93337319+megha-nancy@users.noreply.github.com>
2021-11-10 09:19:47 +0530

    Update Oops_Assignment_Ques_2.java

--- Core_Java_Assignment/Oops_Assignment/Oops_Assignment_Ques_2.java
+++ Core_Java_Assignment/Oops_Assignment/Oops_Assignment_Ques_2.java
@@ -1,41 +1,44 @@
-package oops_assignments;
+public class Bank {
 
-class BankClass{
-	String bankName;
-	int cash;
-	void bankdetails() {
-		System.out.println("This  is "+bankName+" Details");
-	}
-}
-class Saving_account extends BankClass{
-	int Fixed_deposit=8000;
-	void bankdetails() {
-		
-		System.out.println("This  is "+bankName+" Details with Total Cash "+(cash+Fixed_deposit));
-	}
-}
-class Current_account extends BankClass{
-	int Cash_credit=600;
-	void bankdetails() {
-		
-		System.out.println("This  is "+bankName+" Details with Total Cash "+(cash+Cash_credit));
-	}
-}
-
-public class BankMain{
 	public static void main(String[] args) {
-		BankClass sa= new Saving_account();
-		sa.bankName="hdfc";
-		sa.cash=3900;
-		sa.bankdetails();
-		
-		Bank ca= new Current_account();
-		ca.bankName="sbi";
-		ca.cash=2000;
-		ca.bankdetails();
-		
-		
-		
-	}
+   SavingAccount savingaccount = new SavingAccount(20000,1000);
+   CurrentAccount currentaccount = new CurrentAccount(20000,3000);
+   savingaccount.totalAmount();
+   currentaccount.totalAmount();
+   double totalbankbalance= savingaccount.totalAmount()+currentaccount.totalAmt();
+   System.out.println("total cash in the bank:"+totalbankbalance);
+   }
 
 }
+
+public class SavingAccount extends Bank{
+	    private double fixed_Deposit;
+	    private double balance;
+	    
+
+	    public SavingAccount(double fixed_Deposit, double balance)
+	    {
+	    	this.fixed_Deposit=fixed_Deposit;
+	    	this.balance=balance;
+	    }
+	    public double totalAmount() {
+	    	double Savingbalance= fixed_Deposit+balance;
+	    	return Savingbalance;
+	   }
+}
+public class CurrentAccount extends Bank {
+	private double cash_credit;
+	private double balance;
+	
+ public CurrentAccount(double cash_credit, double balance)
+ {
+	 this.balance=balance;
+	 this.cash_credit=cash_credit;
+	 
+ }
+ public double totalAmount() {
+
+     double currentbalance= cash_credit+balance;
+     return currentbalance;
+      }
+	}
