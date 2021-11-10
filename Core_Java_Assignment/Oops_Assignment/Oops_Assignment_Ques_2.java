ead00170de1fb7dc46b70a1f0d5200ec5b64ffcf
megha-nancy <93337319+megha-nancy@users.noreply.github.com>
2021-11-10 09:11:44 +0530

    Update and rename Core_Java_Assignment/Oops_Assignment.txt to Core_Java_Assignment/Oops_Assignment/Oops_Assignment_Ques_2.java

---
+++ Core_Java_Assignment/Oops_Assignment/Oops_Assignment_Ques_2.java
@@ -0,0 +1,41 @@
+package oops_assignments;
+
+class BankClass{
+	String bankName;
+	int cash;
+	void bankDetails() {
+		System.out.println("This  is "+bankName+" Details");
+	}
+}
+class Saving_account extends BankClass{
+	int Fixed_deposit=8000;
+	void bankdetails() {
+		
+		System.out.println("This  is "+bankName+" Details with Total Cash "+(cash+Fixed_deposit));
+	}
+}
+class Current_account extends BankClass{
+	int Cash_credit=600;
+	void bankdetails() {
+		
+		System.out.println("This  is "+bankName+" Details with Total Cash "+(cash+Cash_credit));
+	}
+}
+
+public class BankMain{
+	public static void main(String[] args) {
+		BankClass sa= new Saving_account();
+		sa.bankName="hdfc";
+		sa.cash=3900;
+		sa.bankdetails();
+		
+		Bank ca= new Current_account();
+		ca.bankName="sbi";
+		ca.cash=2000;
+		ca.bankdetails();
+		
+		
+		
+	}
+
+}
